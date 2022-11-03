b15a831d72bba508b5053e66fa87e5b147f998b5
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-11-04 02:40:37 +0800

    Create exercise1_2.cpp

---
+++ ProgrammingSolutions/CPP20/Chapter1/exercise1_2.cpp
@@ -0,0 +1,11 @@
+/* Exercise 1-2: Create and execute a program that output your name on one line
+ * and your age on the next line.
+ */
+#include <iostream>
+
+int main()
+{
+  std::cout << "Name: Judas Nat Pey\n";
+  std::cout << "Age: 100\n";
+  return 0;
+}
