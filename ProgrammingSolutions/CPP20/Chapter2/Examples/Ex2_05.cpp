33a1c2682fcaff2bd92ee952cd16ef7ad4ee88f8
JonesBeach <47111201+hyperjones@users.noreply.github.com>
2022-11-07 01:10:44 +0800

    Create Ex2_05.cpp

---
+++ ProgrammingSolutions/CPP20/Chapter2/Examples/Ex2_05.cpp
@@ -0,0 +1,15 @@
+// Ex2_05.cpp
+// The width, alignment, fill, and 0 formatting options of std::format()
+#include <iostream>
+#include <format>
+
+int main()
+{
+	// Default alignment: right for numbers, left otherwise
+	std::cout << std::format("{:7}|{:7}|{:7}|{:7}|{:7}\n", 1, -.2, "str", 'c', true);
+	// Left and right alignment + custom fill character
+	std::cout << std::format("{:*<7}|{:*<7}|{:*>7}|{:*>7}{:*>7}\n", 1, -2., "str", 'c', true);
+	// Centered alignment + 0 formatting option for numbers
+	std::cout << std::format("{:0>7}|{:0>7}|{:^7}|{:^7}|{:^7};\n", 1, -.2, "str", 'c', true);
+	return 0;
+}
